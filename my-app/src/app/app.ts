87079ca85587a7d2cec787cfd575c0ef6b8e171d
LuisDaSilv <torradeiro@gmail.com>
2025-10-13 11:40:06 +0000

    wip

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  name = "Luis Silva"
+  data:Array<number> = []//[1,2,3,4,5,6]
+  user = false
+
+  getUser(){
+    return "user"
+  }
+
+  handleclick(ev:Event){
+    ev.stopPropagation()
+    this.data.push(this.data.length + 1);
+  }
 }
